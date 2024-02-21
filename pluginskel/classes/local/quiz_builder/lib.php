890ec82506207bfd2f633ab351d96a1f2b166fe3
Zachary DeLawyer <94138524+zachary-delawyer@users.noreply.github.com>
2024-02-21 14:35:59 -0600

    Delete pluginskel directory

--- pluginskel/classes/local/quiz_builder/lib.php
+++
@@ -1,8 +0,0 @@
-defined('MOODLE_INTERNAL') || die();
-
-function local_quiz_builder_extend_navigation(global_navigation $navigation) {
-    $url = new moodle_url('/local/quiz_builder/view.php');
-    $node = navigation_node::create('Quiz Builder', $url, navigation_node::TYPE_CUSTOM, 'quiz_builder', 'quiz_builder', new pix_icon('i/quiz', ''));
-    $navigation->add_node($node);
-    return true;
-}
--- pluginskel/classes/local/quiz_builder/version.php
+++
@@ -1,3 +0,0 @@
-$plugin->version = 2024020600;
-$plugin->requires = 2019111200; // Moodle version required
-$plugin->component = 'qtype_quiz_builder';
